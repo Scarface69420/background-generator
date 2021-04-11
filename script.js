ebfad526e4798ac1808a648fbd2119b809ba6306
Nixon <nixonkurian.nk@gmail.com>
2021-04-11 15:55:21 +0530

    Fixed linear-gradient not showing bug

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+function setGradient() {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-
-function setGradient()  {
-
-	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-	// console.log(body.style.background)
-	css.textContent = body.style.background + ";";
+  css.textContent = body.style.background + ";";
 }
+
+setGradient();
 
 color1.addEventListener("input", setGradient);
 
